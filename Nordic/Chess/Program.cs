813db1f336d669881bbc43ae0454dd42dde9a7fe
Сергей Банников <SBannikov@croc.ru>
2022-04-04 22:12:22 +0300

    Лекция 02

--- Nordic/Chess/Program.cs
+++ Nordic/Chess/Program.cs
@@ -3,35 +3,91 @@
 namespace Chess
 {
     class Program
-    {
+    {        
+        /// <summary>
+        /// Клетка доски
+        /// </summary>
+        const string Cell = "  ";
+
+        /// <summary>
+        /// Главная функция - точка входа в приложение
+        /// </summary>
+        /// <param name="args">Параметры командной строки</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Chess!");
+            Console.WriteLine("Введите размер шахматного поля");
 
-            // Это формирование всего шахматного поля 8*8
-            for (int row = 1; row <= 8; row = row + 1)
+            // Чтение строки из ввода консоли
+            string size = Console.ReadLine();
+
+            // Преобразование строки в целое число
+            bool correct = int.TryParse(size, out int Size);
+            if (!correct)
             {
+                Console.WriteLine($"Я просил число, а не вот это всё: {size}");
+                // Конец выполнения программы
+                return;
+            }
+
+            // Сохранение исходного цвета фона
+            ConsoleColor saveColor = Console.BackgroundColor;
+
+            Console.WriteLine($"Hello Chess {Size}x{Size}");
+
+            // Формирование буквенного обозначения клеток
+            Console.Write($"   ");
+            char c = 'A';
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write($" {c++}");
+            }
+
+            /*
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write($" {label[i]}");
+            }
+            */
+
+            Console.WriteLine();
+            // Это формирование всего шахматного поля 8*8            
+            for (int row = Size; row >= 1; row--)
+            {
+                // Интерполяция строки
+                // вместо {row} подставляется строковый эквивалент переменной row
+                // то есть неявно вызывается метод row.ToString()
+                Console.Write($" {row,2} ");
                 // Это формирование одной строки шахматного поля
-                for (int m = 1; m <= 4; m = m + 1)
+                for (int m = 1; m <= Size / 2; m++)
                 {
                     // Определяем, чётная эта строка или нечётная
                     if (row % 2 == 1)
                     {
                         Console.BackgroundColor = ConsoleColor.Black;
-                        Console.Write("  ");
+                        Console.Write(Cell);
                         Console.BackgroundColor = ConsoleColor.White;
-                        Console.Write("  ");
+                        Console.Write(Cell);
                     }
                     else
                     {
                         Console.BackgroundColor = ConsoleColor.White;
-                        Console.Write("  ");
+                        Console.Write(Cell);
                         Console.BackgroundColor = ConsoleColor.Black;
-                        Console.Write("  ");
+                        Console.Write(Cell);
                     }
                 }
+                // Восстановление исходного цвета фона
+                Console.BackgroundColor = saveColor;
+
+                Console.Write($" {row} ");
+
                 Console.WriteLine();
             }
+                      
+            Console.WriteLine();
+
+            // Восстановление исходного цвета фона
+            Console.BackgroundColor = saveColor;
         }
     }
 }
---
+++ Nordic/DemoApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+/*
+ * Это многострочный комментарий 
+ */
+namespace DemoApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Главная функция - точка входа в приложение
+        /// </summary>
+        /// <param name="args">Параметры командной строки</param>
+        static void Main(string[] args)
+        {
+            double a, b;
+            double number; // вещественное число 
+            double summa; // вещественное число
+            bool flag; // логическое значение
+            string name; // строковое значение
+            string s;
+            char c; // символьное значение
+
+            a = 1;
+            b = 0;
+            number = a + b;
+
+            summa = Math.PI;
+            flag = number > 0.0;
+
+            name = "Это моя программа";
+            s = "!!!!";
+            name = name + s;
+            c = '@';
+
+            Console.WriteLine(number);
+            Console.WriteLine(summa);
+            Console.WriteLine(flag);
+            Console.WriteLine(name);
+            Console.WriteLine(name.Length);
+            Console.WriteLine(c);
+        }
+    }
+}
