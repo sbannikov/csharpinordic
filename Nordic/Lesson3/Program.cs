0d6d638f5ba7f61a87228e34cbdc2bfaf5d0637f
Сергей Банников <SBannikov@croc.ru>
2022-04-11 20:18:06 +0300

    Лекция 04 - промежуточный вариант

--- Nordic/Lesson3/Program.cs
+++ Nordic/Lesson3/Program.cs
@@ -4,35 +4,105 @@ namespace Lesson3
 {
     class Program
     {
-        /// <summary>
-        /// Ширина стакана, без учёта стенок
-        /// </summary>
-        static int Width = 6;
-        /// <summary>
-        /// Высота стакана, без учёта донышка
-        /// </summary>
-        static int Height = 10;
-        /// <summary>
-        /// Уровень заполнения стакана "жидкостью"
-        /// </summary>
-        static int Level = 9;
 
+        /// <summary>
+        /// Запрос числа у пользователя с контролем значений
+        /// </summary>
+        /// <param name="prompt">Экранная подсказка</param>
+        /// <param name="max">Максимальное допустимое число (если 0, то контроль не выполняется)</param>
+        /// <returns>Натуральное число</returns>
+        static int ReadNumber(string prompt, int max)
+        {
+            bool correct;
+            int number;
+            do
+            {
+                if (max == 0)
+                {
+                    Console.Write($"{prompt} (от 1) > ");
+                }
+                else
+                {
+                    Console.Write($"{prompt} (от 1 до {max}) > ");
+                }
+
+                // Чтение строки из ввода консоли
+                string s = Console.ReadLine();
+                // Преобразование строки в целое число
+                correct = int.TryParse(s, out number);
+                if (!correct)
+                {
+                    Console.WriteLine($"Я просил число, а не вот это всё: {s}");
+                }
+                // Проверка числа на корректность
+                if (number <= 0)
+                {
+                    correct = false;
+                    Console.WriteLine($"Я просил натуральное число (больше 0), а не вот это всё: {s}");
+                }
+                else if (number > max && max != 0)
+                {
+                    correct = false;
+                    Console.WriteLine($"Я просил число не больше {max}, а не вот это всё: {s}");
+                }
+            } while (!correct);
+            return number;
+        }
+
+        /// <summary>
+        /// Расчет цвета жидкости
+        /// </summary>
+        /// <param name="ratio">Коэффициент заполнения от 0 до 1</param>
+        /// <returns></returns>
+        static ConsoleColor GetColor(double ratio)
+        {
+            ConsoleColor result;
+            if (ratio > 2.0 / 3) // важно, что деление вещественное
+            {
+                result = ConsoleColor.Red;
+            }
+            else if (ratio > 1.0 / 3) // важно, что деление вещественное
+            {
+                result = ConsoleColor.DarkGreen;
+            }
+            else
+            {
+                result = ConsoleColor.Blue;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args">Список параметров</param>
         static void Main(string[] args)
         {
+            // Ширина стакана, без учёта стенок
+            int Width;
+            // Высота стакана, без учёта донышка
+            int Height;
+            /// Уровень заполнения стакана "жидкостью"
+            int Level;
+
+            Width = ReadNumber("Введите ширину стакана", 0);
+            Height = ReadNumber("Введите высоту стакана", Console.WindowHeight);
+            Level = ReadNumber("Введите уровень жидкости", Height);
             // Сохранить цвет текста консоли
             ConsoleColor color = Console.ForegroundColor;
             // Кнопка, которую нажимает человек
             ConsoleKeyInfo key;
+            // стереть всё на экране
+            Console.Clear();
 
             do
             {
-                // стереть всё на экране
-                // Console.Clear();
                 // Перемещение курсора в верхний левый угол
                 Console.CursorLeft = 0;
                 Console.CursorTop = 0;
 
-
+                // Один раз вычисляем цвет жидкости
+                ConsoleColor waterColor = GetColor(Level / (double)Height);
                 // Повторение 10 раз -- формируем строки стакана
                 for (int i = Height; i >= 1; i--)
                 {
@@ -43,18 +113,7 @@ namespace Lesson3
                         if (i <= Level) // если это уже "жидкость"
                         {
                             // рисуем жидкость разными цветами
-                            if (Level > Height * 2 / 3)
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                            }
-                            else if (Level > Height / 3)
-                            {
-                                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                            }
-                            else
-                            {
-                                Console.ForegroundColor = ConsoleColor.Blue;
-                            }
+                            Console.ForegroundColor = waterColor;
                             Console.Write(".");
                         }
                         else
