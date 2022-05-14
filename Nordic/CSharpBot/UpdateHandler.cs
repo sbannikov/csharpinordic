abb225cef1f838161f89e17d6042a4ac9855eb98
Сергей Банников <SBannikov@croc.ru>
2022-05-14 16:23:04 +0300

    Обработка основных команд

--- Nordic/CSharpBot/UpdateHandler.cs
+++ Nordic/CSharpBot/UpdateHandler.cs
@@ -23,6 +23,11 @@ namespace CSharpBot
         private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
         /// <summary>
+        /// Словарь пользователей
+        /// </summary>
+        private Dictionary<long, User> users = new Dictionary<long, User>();
+
+        /// <summary>
         /// Обработка ошибок
         /// </summary>
         /// <param name="botClient"></param>
@@ -71,9 +76,14 @@ namespace CSharpBot
             switch (message.Type)
             {
                 case MessageType.Text:
-                    // обработка результата?
-                    сlient.SendTextMessageAsync(message.Chat.Id, $"вы сказали мне: {message.Text}");
-                    log.Trace(message.Text);
+                    if (message.Text[0] == '/')
+                    {
+                        ProcessCommand(сlient, message);
+                    }
+                    else
+                    {
+                        ProcessText(сlient, message);
+                    }                    
                     break;
 
                 default:
@@ -82,6 +92,63 @@ namespace CSharpBot
                     log.Warn(s);
                     break;
             }
+        }
+
+        /// <summary>
+        /// Обработка команды
+        /// </summary>
+        /// <param name="сlient"></param>
+        /// <param name="message"></param>
+        private void ProcessCommand(ITelegramBotClient сlient, Message message)
+        {
+            // Команда
+            string command = message.Text.Substring(1).ToLower();
+
+            switch (command)
+            {
+                case "start":
+                    if (!users.ContainsKey(message.Chat.Id))
+                    {
+                        // Добавление пользователя в словарь
+                        var user = new User()
+                        {
+                            ID = message.Chat.Id
+                        };
+                        users.Add(message.Chat.Id, user);
+                        сlient.SendTextMessageAsync(message.Chat.Id, $"{message.Chat.FirstName}, я вас зарегистрировал");
+                    }
+                    else
+                    {
+                        сlient.SendTextMessageAsync(message.Chat.Id, $"{message.Chat.FirstName}, вы уже зарегистрированы");
+                    }
+                    break;
+                case "play":
+                    // [!]
+                    break;
+                case "help":
+                    сlient.SendTextMessageAsync(message.Chat.Id, "Цель игры - разблокировать 12-й этаж");
+                    break;
+                case "about":
+                    сlient.SendTextMessageAsync(message.Chat.Id, "Учебный бот на C# - текстовый квест");
+                    break;
+                default:
+                    string s = $"Команда {command} пока не обрабатываются";
+                    сlient.SendTextMessageAsync(message.Chat.Id, s);
+                    log.Warn(s);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Обработка текста
+        /// </summary>
+        /// <param name="сlient"></param>
+        /// <param name="message"></param>
+        private void ProcessText(ITelegramBotClient сlient, Message message)
+        {
+            // обработка результата?
+            сlient.SendTextMessageAsync(message.Chat.Id, $"{message.Chat.FirstName}, вы сказали мне: {message.Text}");
+            log.Trace(message.Text);
         }
     }
 }
---
+++ Nordic/CSharpBot/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBot
+{
+    /// <summary>
+    /// Пользователь Telegram
+    /// </summary>
+    public class User
+    {
+       /// <summary>
+       /// Идентификатор пользователя
+       /// </summary>
+       public long ID;
+    }
+}
