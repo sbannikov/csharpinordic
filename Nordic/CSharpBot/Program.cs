a6e90c35bccdc883607401958b1c3c06dd56666a
Сергей Банников <SBannikov@croc.ru>
2022-05-14 15:34:48 +0300

    Каркас приёма сообщений бота

--- Nordic/CSharpBot/Program.cs
+++ Nordic/CSharpBot/Program.cs
@@ -1,24 +1,35 @@
 ﻿using System;
 using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using NLog;
 
 namespace CSharpBot
 {
     internal class Program
     {
+        /// <summary>
+        /// Протоколирование
+        /// </summary>
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Бот запускается...");
+            log.Info("Бот запускается...");
             // Создание клиента
             var client = new TelegramBotClient("<TOKEN>");
             // Запрос информации о самом боте
             var result = client.GetMeAsync();
-            Console.WriteLine("Запрос отправлен");
+            log.Trace("Запрос отправлен");
             // Ожидание результата
             result.Wait();
             // Получение результата асинхронного запроса
             var user = result.Result;
             // Вывод сведений о боте
-            Console.WriteLine($"Бот {user.Username} отвечает, следовательно, он существует");
+            log.Info($"Бот {user.Username} отвечает, следовательно, он существует");
+            // Начало приёма сообщений
+            var handler = new UpdateHandler();           
+            client.StartReceiving(handler);
+
             Console.ReadLine();
         }
     }
---
+++ Nordic/CSharpBot/UpdateHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using NLog;
+
+namespace CSharpBot
+{
+    /// <summary>
+    /// Обработчик событий от бота
+    /// </summary>
+    public class UpdateHandler : IUpdateHandler
+    {
+        /// <summary>
+        /// Протоколирование
+        /// </summary>
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Обработка ошибок
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="exception"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            log.Warn(exception);
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Обработка сообщений боту
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            log.Trace(update);
+            return Task.CompletedTask;
+        }
+    }
+}
