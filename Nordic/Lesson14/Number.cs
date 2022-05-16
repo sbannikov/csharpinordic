705fca8524c9876173819e3491db91e208b3576e
Сергей Банников <SBannikov@croc.ru>
2022-05-16 21:00:56 +0300

    Конструкторы и деструкторы

---
+++ Nordic/Lesson14/Number.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson14
+{
+    /// <summary>
+    /// Случайное целое число
+    /// </summary>
+    public class Number
+    {
+        /// <summary>
+        /// Генератор случайных чисел
+        /// </summary>
+        private static readonly Random random;
+
+        /// <summary>
+        /// Счётчик созданных объектов
+        /// </summary>
+        public static int counter = 0;
+
+        /// <summary>
+        /// Целое число
+        /// </summary>
+        public readonly int number;
+
+        /// <summary>
+        /// Статический конструктор
+        /// </summary>
+        static Number()
+        {
+            Console.WriteLine("static Number()");
+            random = new Random();
+        }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Number() : this(random.Next(0, 1000))
+        {
+        }
+
+        /// <summary>
+        /// Конструктор по заданному числу
+        /// </summary>
+        /// <param name="n">Натуральное число</param>
+        public Number(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException("n");
+            }
+            number = n;
+            Console.WriteLine($"+ {number}");
+            counter++;
+        }
+
+        /// <summary>
+        /// Деструктор
+        /// </summary>
+        ~Number()
+        {
+            counter--;
+            Console.WriteLine($"- {number}, осталось {counter} объектов");
+        }
+    }
+}
---
+++ Nordic/Lesson14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson14
+{
+    internal class Program
+    {
+        static void Process()
+        {
+            List<Number> list = new List<Number>();
+            for (int n = 0; n < 10; n++)
+            {
+                Console.WriteLine($"n = {n}");
+                list.Add(new Number());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Process();
+            // Принудительная сборка мусора
+            // GC.Collect();
+            Console.ReadLine();
+        }
+    }
+}
