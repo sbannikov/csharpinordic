622c3988be7a7b9ed8215ddd3371385207effdbb
Сергей Банников <SBannikov@croc.ru>
2022-05-21 17:05:58 +0300

    Обработка условий

--- Nordic/CSharpBot/Quest/Action.cs
+++ Nordic/CSharpBot/Quest/Action.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using NLog;
 
 namespace CSharpBot.Quest
 {
     public class Action
     {
+        /// <summary>
+        /// Протоколирование
+        /// </summary>
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// Название действия
         /// </summary>
@@ -39,7 +46,50 @@ namespace CSharpBot.Quest
         /// <returns></returns>
         public bool IsCondition(User user)
         {
-            return true;
+            // Если условие не задано,  значит оно всегда истинно
+            if (string.IsNullOrEmpty(Condition)) return true;
+            // Очистка пробелов
+            string c = Condition.Replace(" ", "");
+            // группы: (1)     (2) (3)
+            // строка: undress ==  0
+            var match = Regex.Match(c, "^([a-z]+)(==|!=|>|<|>=|<=)([0-9]+)$", RegexOptions.IgnoreCase);
+            // Если не соответствует грамматике, то условие не выполнено
+            if (!match.Success)
+            {
+                log.Warn($"Некорректное условие: {Condition}");
+                return false;
+            }
+            string name = match.Groups[1].Value;
+            string operation = match.Groups[2].Value;
+            string constant = match.Groups[3].Value;
+
+            // Получение значения переменной игрока по имени
+            int value = 0;
+            if (user.Variables.TryGetValue(name, out int i))
+            {
+                value = i;
+            }
+
+            int constValue = int.Parse(constant);
+
+            switch (operation)
+            {
+                case "==":
+                    return value == constValue;
+                case "!=":
+                    return value != constValue;
+                case ">=":
+                    return value >= constValue;
+                case ">":
+                    return value > constValue;
+                case "<=":
+                    return value <= constValue;
+                case "<":
+                    return value < constValue;
+                default:
+                    log.Warn($"Некорректная операция: {Condition}");
+                    return false;
+            }
         }
     }
 }
