3dfb8dd733a758e88060d0307a98a0f1043eb3f1
Сергей Банников <SBannikov@croc.ru>
2022-05-23 20:56:04 +0300

    Добавление и таскание фигурки

--- Nordic/Turnip/MainWindow.xaml.cs
+++ Nordic/Turnip/MainWindow.xaml.cs
@@ -21,6 +21,21 @@ namespace Turnip
     public partial class MainWindow : Window
     {
         /// <summary>
+        /// Генератор случайных чисел
+        /// </summary>
+        private Random random = new Random();
+
+        /// <summary>
+        /// Признак нажатия левой кнопки мыши
+        /// </summary>
+        private bool mousePressed = false;
+
+        /// <summary>
+        /// Точка, где мышка была нажата
+        /// </summary>
+        private Point mouseDelta;
+
+        /// <summary>
         /// Сценарий игры из файла JSON
         /// </summary>
         private Scenario.Game game;
@@ -42,6 +57,85 @@ namespace Turnip
         {
             // Загрузка сценария
             game = Scenario.Game.Load<Scenario.Game>();
+
+            // Начальное формирование кнопок
+            foreach (string name in game.StartThings)
+            {
+                // Создаем кнопку
+                var button = new Button()
+                {
+                    Content = name
+                };
+                button.Click += AddThing_Click;
+                toolBar.Items.Add(button);
+            }
+        }
+
+        private const int ThingWidth = 100;
+        private const int ThingHeight = 100;
+
+        /// <summary>
+        /// Добавление нового предмета на поле
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void AddThing_Click(object sender, RoutedEventArgs e)
+        {
+            var rect = new Rectangle()
+            {
+                Height = ThingWidth,
+                Width = ThingHeight,
+                Fill = new SolidColorBrush(Colors.Navy)
+            };
+
+            // Обработчики для перемещения
+            rect.MouseDown += Thing_MouseDown;
+            rect.MouseUp += Thing_MouseUp;
+            rect.MouseMove += Thing_MouseMove;
+
+            // Устанавливаем в случайное место 
+            Canvas.SetLeft(rect, random.Next(0, (int)(canvas.ActualWidth - ThingWidth)));
+            Canvas.SetTop(rect, random.Next(0, (int)(canvas.ActualHeight - ThingHeight)));
+
+            canvas.Children.Add(rect);
+        }
+
+        /// <summary>
+        /// Перемещение мыши
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void Thing_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousePressed)
+            {
+                Point point = e.GetPosition(canvas);
+                Canvas.SetLeft((UIElement)sender, point.X - mouseDelta.X);
+                Canvas.SetTop((UIElement)sender, point.Y - mouseDelta.Y);
+            }
+        }
+
+        /// <summary>
+        /// Фиксация отжатия левой кнопки мыши
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Thing_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            mousePressed = false;
+        }
+
+        /// <summary>
+        /// Фиксация нажатия левой кнопки мыши
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Thing_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mousePressed = true;
+            mouseDelta = e.GetPosition((UIElement)sender);
         }
     }
 }
