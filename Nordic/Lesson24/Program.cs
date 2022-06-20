dd9b1b9e2b99a1cda82895f2077cc9dd4e0f772c
Сергей Банников <SBannikov@croc.ru>
2022-06-20 19:42:48 +0300

    Факториал - вещественные числа

--- Nordic/Lesson24/Program.cs
+++ Nordic/Lesson24/Program.cs
@@ -7,16 +7,16 @@ namespace Lesson24
     {
         static void Main(string[] args)
         {
-            int f = Calculation.Factorial(5);
-            for (int i = 0; i <= 10; i++)
+            // int f = Calculation.Factorial(5);
+            for (int i = 0; i <= 200; i++)
             {
                 DateTime t0 = DateTime.Now;
-                int f1 = Calculation.Factorial(i);
+                double f1 = Calculation.FactorialD(i);
                 TimeSpan t1 = DateTime.Now - t0;
                 t0 = DateTime.Now;
-                int f2 = Calculation.FactorialLoop(i);
+                double f2 = Calculation.FactorialLoopD(i);
                 TimeSpan t2 = DateTime.Now - t0;
-                Console.WriteLine($"{i}! = {f1} {f2}, {t1} {t2}");  
+                Console.WriteLine($"{i}! = {f1} {f2}, {t1.TotalMilliseconds} {t2.TotalMilliseconds}");  
             }
         }
     }
--- Nordic/NordicCommon/Calculation.cs
+++ Nordic/NordicCommon/Calculation.cs
@@ -46,5 +46,44 @@ namespace NordicCommon
             }
             return factorial;
         }
+
+        /// <summary>
+        /// Факториал методом рекурсии
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static double FactorialD(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException($"Отрицательные значения n не поддерживаются: {n}");
+            }
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * FactorialD(n - 1);
+        }
+
+        /// <summary>
+        /// Факториал методом цикла
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static double FactorialLoopD(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException($"Отрицательные значения n не поддерживаются: {n}");
+            }
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
     }
 }
