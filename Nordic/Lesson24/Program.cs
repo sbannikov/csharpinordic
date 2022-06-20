86096ee8649f3de37767c74652c3c5ec886aa1d3
Сергей Банников <SBannikov@croc.ru>
2022-06-20 19:35:28 +0300

    Лекция 24 - факториал целых чисел

---
+++ Nordic/Lesson24/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NordicCommon;
+
+namespace Lesson24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = Calculation.Factorial(5);
+            for (int i = 0; i <= 10; i++)
+            {
+                DateTime t0 = DateTime.Now;
+                int f1 = Calculation.Factorial(i);
+                TimeSpan t1 = DateTime.Now - t0;
+                t0 = DateTime.Now;
+                int f2 = Calculation.FactorialLoop(i);
+                TimeSpan t2 = DateTime.Now - t0;
+                Console.WriteLine($"{i}! = {f1} {f2}, {t1} {t2}");  
+            }
+        }
+    }
+}
---
+++ Nordic/NordicCommon/Calculation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NordicCommon
+{
+    public static class Calculation
+    {
+        /// <summary>
+        /// Факториал методом рекурсии
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static int Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException($"Отрицательные значения n не поддерживаются: {n}");
+            }
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        /// <summary>
+        /// Факториал методом цикла
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static int FactorialLoop(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException($"Отрицательные значения n не поддерживаются: {n}");
+            }
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
