665af0768c2f4469d88da388536b61136d85e2ea
Сергей Банников <SBannikov@croc.ru>
2022-07-16 15:56:40 +0300

    Каркас приложения юдля сбора погоды

---
+++ Nordic/Weather/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace Weather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var run = HostFactory.Run(x =>
+            {
+                x.Service<Worker>(s => 
+                {
+                    s.ConstructUsing(w => new Worker());
+                    s.WhenStarted(w => w.StartService());
+                    s.WhenStopped(w => w.StopService());
+                    s.WhenPaused(w => w.PauseService());
+                    s.WhenContinued(w => w.ContinueService());
+                });
+                x.RunAsLocalSystem();
+                x.SetServiceName("weathersvc");
+                x.SetDisplayName("Служба сбора метеорологических данных");
+                x.SetDescription("Служба сбора метеорологических данных от Яндекса");
+            });
+        }
+    }
+}
---
+++ Nordic/Weather/Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public class Worker
+    {
+        public Worker()
+        {
+        }
+
+        public void StartService()
+        {
+        }
+
+        public void StopService()
+        {
+        }
+
+        public void PauseService()
+        { 
+        }
+
+        public void ContinueService()
+        { 
+        }
+    }
+}
