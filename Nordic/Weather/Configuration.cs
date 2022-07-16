06abb6dbe20aa7c29ee38943f57f2e830b24f28d
Сергей Банников <SBannikov@croc.ru>
2022-07-16 16:17:14 +0300

    Получение данных о погоде

---
+++ Nordic/Weather/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Weather
+{
+    /// <summary>
+    /// Конфигурация приложения
+    /// </summary>
+    public class Configuration : JsonFile
+    {
+        /// <summary>
+        /// Ключ доступа к API Yandex Погода
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// Интервал в секундах
+        /// </summary>       
+        public int IntervalInSeconds { get; set; } = 1;
+    }
+}
--- Nordic/Weather/Worker.cs
+++ Nordic/Weather/Worker.cs
@@ -3,29 +3,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Weather
 {
     public class Worker
     {
+        private HttpClient client;
+        private string key;
+        private System.Timers.Timer timer;
+
         public Worker()
         {
+            client = new HttpClient();
+
+            // Загрузка конфигурации из файла
+            // [!] выполняется однократно, при запуске
+            // если файл конфигурации поменяется, придётся перезапускать сервис
+            var config = JsonFile.Load<Configuration>();
+            key = config.ApiKey;
+
+            timer = new System.Timers.Timer(config.IntervalInSeconds * 1000);
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        /// <summary>
+        /// Основное событие запроса данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                timer.Stop();
+
+                UriBuilder uri = new UriBuilder("https://api.weather.yandex.ru/v2/forecast");
+                var parameters = HttpUtility.ParseQueryString(string.Empty);
+                // Ханты-Мансийск
+                parameters["lat"] = "61.003184";
+                parameters["lon"] = "69.018911";
+                parameters["lang"] = "ru_RU";
+                parameters["limit"] = "7";
+                parameters["hours"] = "true";
+                parameters["extra"] = "true";
+                uri.Query = parameters.ToString();
+
+                // Запрос к облаку
+                var request = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(uri.ToString())
+                };
+
+                // Авторизация запроса 
+                request.Headers.Add("X-Yandex-API-Key", key);
+
+                // Выполнение запроса
+                HttpResponseMessage response = client.Send(request);
+
+                string result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    result = response.ReasonPhrase ?? string.Empty;
+                }
+                else
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    //              result = JsonSerializer.Deserialize<Response>(json).Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                // [!] дописать на досуге
+            }
+            finally
+            {
+                timer.Start();
+            }
         }
 
         public void StartService()
         {
+            timer.Start();
         }
 
         public void StopService()
         {
+            timer.Stop();
         }
 
         public void PauseService()
-        { 
+        {
+            timer.Stop();
         }
 
         public void ContinueService()
-        { 
+        {
+            timer.Start();
         }
     }
 }
